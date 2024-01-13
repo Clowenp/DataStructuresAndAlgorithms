a73dac49541f77827a1ceab2bb36a3bc03c643bd
Owen P <surfo8970bb@gmail.com>
2024-01-13 00:02:01 -0500

    2024-01-13

--- LeetCode75s/DFS/Medium/437PathSum.cc
+++ LeetCode75s/DFS/Medium/437PathSum.cc
@@ -25,6 +25,7 @@ The number of nodes in the tree is in the range [0, 1000].
 */
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -44,7 +45,7 @@ struct TreeNode {
 
 
 // WAY TOO SLOW
-class Solution {
+class SlowSolution {
 
     int pathStartingAtNode (TreeNode* node, int sum, int targetSum) {
         if (!node) return 0;
@@ -65,6 +66,103 @@ public:
         return pathStartingAtNode(root, 0, targetSum) + pathSum(root->left, targetSum) + pathSum(root->right, targetSum);
     }
 };
+
+// Somehow working??? Same solution except answer is stored
+class SolutionProper {
+    int ans = 0;
+
+    void pathStartingAtNode (TreeNode* node, double sum, int targetSum) {
+        if (!node) return;
+        if (node->val + sum == targetSum) {
+            ++ans;
+        } 
+        pathStartingAtNode(node->left, node->val+sum, targetSum); 
+        pathStartingAtNode(node->right, node->val+sum, targetSum);
+    }   
+
+public:
+    int pathSum(TreeNode* root, int targetSum) {
+        if (!root) return 0;
+
+        pathStartingAtNode(root, 0, targetSum);
+        pathSum(root->left, targetSum);
+        pathSum(root->right, targetSum);
+        return ans;
+    }
+};
+
+// ==================================== RELATED ========================================
+
+// faster solution than 1st version
+class SolutionWorkingSlow {
+    int pathSum(TreeNode* node, int targetSum, vector<int> path) {
+        if (!node) return 0;
+        path.emplace_back(node->val);
+        int validPaths = 0;
+        double sum = 0;
+        for (int i = path.size() - 1; i >= 0; --i) {
+            sum += path[i];
+            if (sum == targetSum) {
+                ++validPaths;
+            }
+        }
+        return validPaths + pathSum(node->left, targetSum, path) + pathSum(node->right, targetSum, path);
+    }
+public:
+    int pathSum(TreeNode* root, int targetSum) {
+        return pathSum(root, targetSum, vector<int>{});
+    }
+};
+
+// Possible faster? NOOO, how.
+class SolutionNotFaster {
+    int ans = 0;
+    void pathSum(TreeNode* node, int targetSum, vector<int> path) {
+        if (!node) return ;
+        path.emplace_back(node->val);
+        int validPaths = 0;
+        double sum = 0;
+        for (int i = path.size() - 1; i >= 0; --i) {
+            sum += path[i];
+            if (sum == targetSum) {
+                ++validPaths;
+            }
+        }
+        ans += validPaths;
+        pathSum(node->left, targetSum, path);
+        pathSum(node->right, targetSum, path);
+    }
+public:
+    int pathSum(TreeNode* root, int targetSum) {
+        pathSum(root, targetSum, vector<int>{});
+        return ans;
+    }
+};
+
+class Solution {
+    int ans = 0;
+    vector<int> path;
+public:
+    int pathSum(TreeNode* root, int targetSum) {
+        if (!root) return 0;
+        path.emplace_back(root->val);
+        int validPaths = 0;
+        double sum = 0;
+        for (int i = path.size() - 1; i >= 0; --i) {
+            sum += path[i];
+            if (sum == targetSum) {
+                ++validPaths;
+            }
+        }
+        ans += validPaths;
+        pathSum(root->left, targetSum);
+        pathSum(root->right, targetSum);
+        path.erase(path.begin() + path.size() - 1);
+        return ans;
+    }
+};
+
+// ==================================== END RELATED ========================================
 
 int main () {
     TreeNode tree1 {1, new TreeNode {2, nullptr, nullptr}, new TreeNode {3, nullptr, nullptr}};
